7d3c2756429532bf416c81d7b7098ef6abb42e57
Catalina <caliste@fen.uchile.cl>
2020-08-29 12:50:37 -0400

    Limitar vista para ususuarios no registrados

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,5 +1,4 @@
 class LikesController < ApplicationController
-
   def index
     @likes = Likes.all
   end
@@ -17,27 +16,27 @@ class LikesController < ApplicationController
 
   def create
     #pasos para obtener el id de tweets a través de format (hash que "manda")
-    tweet = Tweet.find_by(id:params[:format])
-    tweet_id = tweet.id
-    #obtener el id de user a través del helper de devise
-    user_id = current_user.id
-    tiene_like = Like.where(user_id: user_id, tweet_id: tweet_id).present?
+      tweet = Tweet.find_by(id:params[:format])
+      tweet_id = tweet.id
+      #obtener el id de user a través del helper de devise
+      user_id = current_user.id
+      tiene_like = Like.where(user_id: user_id, tweet_id: tweet_id).present?
 
-    #luego de tener al tweet y user asociado, hay que determinar si existe un registro de ese like
-    #considerar que el registro sea mayor a 0, sino quedará negativo
-    if tiene_like && tweet.likes_count > 0
-      #si tiene like, entonces que lo elimine del registro de la tabla tweet
-      tweet.likes_count-=1
-    else
-      #se crea un like con el usuario y tweet correspondiente
-      Like.create(user_id:user_id, tweet_id:tweet_id)
-      #se traspasa la info a la tabla tweet
-      tweet.likes_count+=1
-    end
-    #para recargar la página con los nuevos likes/dislikes
-    redirect_back(fallback_location: root_path)
-    #para guardar en la tabla de tweets
-    tweet.save
+      #luego de tener al tweet y user asociado, hay que determinar si existe un registro de ese like
+      #considerar que el registro sea mayor a 0, sino quedará negativo
+      if tiene_like && tweet.likes_count > 0
+        #si tiene like, entonces que lo elimine del registro de la tabla tweet
+        tweet.likes_count-=1
+      else
+        #se crea un like con el usuario y tweet correspondiente
+        Like.create(user_id:user_id, tweet_id:tweet_id)
+        #se traspasa la info a la tabla tweet
+        tweet.likes_count+=1
+      end
+      #para recargar la página con los nuevos likes/dislikes
+      redirect_back(fallback_location: root_path)
+      #para guardar en la tabla de tweets
+      tweet.save
   end
 
   def update
